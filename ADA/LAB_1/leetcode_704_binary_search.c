23c6cc677ec39e495af255fec1d7e7d31c8f20ca
ShrihariViswanathan <shrihariviswanathan.cs24@bmsce.ac.in>
2026-02-26 09:37:04 +0530

    Update leetcode_704_binary_search.c

--- ADA/LAB_1/leetcode_704_binary_search.c
+++ ADA/LAB_1/leetcode_704_binary_search.c
@@ -1,18 +1,20 @@
 int search(int* nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize - 1;
-    
-    while (left < right){
-        int mid = (left + right) / 2;
-        if (nums[mid] == target)
-            return mid;
-        else if (target > nums[left]){
-            left = mid + 1;
-        } 
-        else if (target < nums[right]){
-            right = mid - 1;
-        }
 
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;     // move right
+        }
+        else {
+            right = mid - 1;    // move left
+        }
     }
+
     return -1;
 }
